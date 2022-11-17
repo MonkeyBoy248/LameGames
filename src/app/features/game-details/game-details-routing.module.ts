0ad07537c27dc6070a5ee5f11fa92e468ebb8943
Kirill Temnyi <kirik201100@mail.ru>
2022-11-17 11:14:23 +0300

    refactor: remove search component

--- src/app/features/game-details/game-details-routing.module.ts
+++ src/app/features/game-details/game-details-routing.module.ts
@@ -4,14 +4,12 @@ import { GameDetailsComponent } from './game-details.component';
 import {AboutComponent} from "./pages/about/about.component";
 import {ScreenshotsComponent} from "./pages/screenshots/screenshots.component";
 import {TrailersComponent} from "./pages/trailers/trailers.component";
-import {SearchPageComponent} from "../home/pages/search-page/search-page.component";
 
 const routes: Routes = [
   {path: '', component: GameDetailsComponent, children: [
     { path: 'about', component: AboutComponent },
     { path: 'screenshots', component: ScreenshotsComponent },
     { path: 'trailers', component: TrailersComponent },
-      { path: 'search', component: SearchPageComponent }
   ]},
 ];
 
