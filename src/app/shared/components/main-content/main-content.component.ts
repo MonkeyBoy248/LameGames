8c13a425f5753fcab2d6e56e71bb5c92744c9c0b
Kirill Temnyi <kirik201100@mail.ru>
2022-11-17 11:07:08 +0300

    refactor: change the sorting logic

--- src/app/shared/components/main-content/main-content.component.ts
+++ src/app/shared/components/main-content/main-content.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-main-content',
@@ -10,9 +12,21 @@ export class MainContentComponent implements OnInit {
   @Input() data!: any;
   @Input() isGrid: boolean = true;
 
-  constructor() { }
+  sortingOption$: Observable<string>;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sortingOption$ = this.route.queryParamMap.pipe(
+      map((params) => {
+        if (!params.get('filter')) {
+          return 'name'
+        }
+
+        return params.get('filter')!
+      }),
+      tap(param => console.log(param))
+    )
   }
 
   trackById (index: number, data: any): number {
