ae999a07491e882d4fcf5efcdd0d8d1af5156880
Kirill Temnyi <kirik201100@mail.ru>
2022-11-17 11:07:58 +0300

    refactor: remove search component

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -4,14 +4,12 @@ import { HomeComponent } from './home.component';
 import {GamesPageComponent} from "./pages/games-page/games-page.component";
 import {DevPageComponent} from "./pages/dev-page/dev-page.component";
 import {GamePlatformsPageComponent} from "./pages/game-platforms-page/game-platforms-page.component";
-import {SearchPageComponent} from "./pages/search-page/search-page.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent, children: [
     { path: 'games', component: GamesPageComponent },
     { path: 'developers', component: DevPageComponent},
     { path: 'platforms', component: GamePlatformsPageComponent},
-    { path: 'search', component: SearchPageComponent },
   ] },
 ];
 
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import {CoreModule} from "../../core/core.module";
 import {SharedModule} from "../../shared/shared.module";
 import {GamesPageComponent} from "./pages/games-page/games-page.component";
-import {GamePlatformsService} from "../../core/services/game-platforms/game-platforms.service";
 import {GamePlatformsPageComponent} from "./pages/game-platforms-page/game-platforms-page.component";
 import {DevPageComponent} from "./pages/dev-page/dev-page.component";
-import {SearchPageComponent} from "./pages/search-page/search-page.component";
 import {FilterSelectorComponent} from "./components/filter-selector/filter-selector.component";
 
 
@@ -19,7 +16,6 @@ import {FilterSelectorComponent} from "./components/filter-selector/filter-selec
     GamesPageComponent,
     GamePlatformsPageComponent,
     DevPageComponent,
-    SearchPageComponent,
     FilterSelectorComponent
   ],
   imports: [
